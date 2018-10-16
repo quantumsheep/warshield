0950b859c987dbc7b2459cb2d7a10618887c9850
QuantumSheep <quantumsheep@outlook.fr>
2018-10-16 17:04:17 +0200

    Add more error handling and verbosity

--- helpers.js
+++ helpers.js
@@ -7,12 +7,27 @@ const walk = async (dir, filelist = []) => {
 
   for (file of files) {
     const filepath = path.join(dir, file);
-    const stat = await util.promisify(fs.stat)(filepath);
 
-    if (stat.isDirectory()) {
-      filelist = await walk(filepath, filelist);
-    } else {
-      filelist.push(filepath);
+    try {
+      const stat = await util.promisify(fs.stat)(filepath);
+
+      if (stat.isDirectory()) {
+        filelist = await walk(filepath, filelist);
+      } else {
+        filelist.push(filepath);
+
+        if (filepath.length > 50) {
+          console.log(`Added in file queue: ${filepath.slice(0, 25)}[...]${filepath.slice(-25)}`);
+        } else {
+          console.log(`Added in file queue: ${filepath}`);
+        }
+      }
+    } catch (e) {
+      if (filepath.length > 50) {
+        console.log(`\x1b[31mNot added in file queue: ${filepath.slice(0, 25)}[...]${filepath.slice(-25)}\x1b[0m`);
+      } else {
+        console.log(`\x1b[31mNot added in file queue: ${filepath}\x1b[0m`);
+      }
     }
   }
 
