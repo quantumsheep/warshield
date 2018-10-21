cd2839de0ef26d6057fb7c92eaf12d01cc8fb621
QuantumSheep <nathanael.dmc@outlook.fr>
2018-10-21 16:49:44 +0200

    Add IV, AuthTag, random rounds and use AES-256-GCM

--- wall.js
+++ wall.js
@@ -1,98 +1,149 @@
 const fs = require('fs');
+const util = require('util');
 const crypto = require('crypto');
 
-// Define a fixed IV to match AES recommendations
-// Full Buffer is d0 fd 1a 44 c4 fb ed d1 56 27 3f 85 60 51 0f 1a
-const IV = Buffer.from([
-  0xd0, 0xfd, 0x1a, 0x44,
-  0xc4, 0xfb, 0xed, 0xd1,
-  0x56, 0x27, 0x3f, 0x85,
-  0x60, 0x51, 0x0f, 0x1a,
-]).toString('hex').slice(0, 16);
-
-const ENCRYPTION_ALGORITHM = 'aes-256-cbc';
+const ENCRYPTION_ALGORITHM = 'aes-256-gcm';
+const MIN_ROUNDS = 3000;
+const MAX_ROUNDS = 9000;
 
 /**
  * @param {string | Buffer | NodeJS.TypedArray | DataView} key 
+ * @param {number} rounds 
+ * @param {string | Buffer | NodeJS.TypedArray | DataView} salt 
  */
-const generateKey = key => new Promise((resolve, reject) => {
-  crypto.pbkdf2(key, 'salt', 100000, 32, 'sha512', (err, derivedKey) => {
+const generateKey = (key, rounds, salt = null) => new Promise((resolve, reject) => {
+  salt = salt || crypto.randomBytes(64);
+
+  crypto.pbkdf2(key, salt, rounds, 32, 'sha512', (err, derivedKey) => {
     if (err) return reject(err);
 
-    resolve(derivedKey);
+    resolve([derivedKey, salt]);
   });
 });
 
 /**
- * @param {Buffer} key 
+ * @param {string | Buffer | NodeJS.TypedArray | DataView} key 
  */
 const encryptStream = (key) => {
-  return crypto.createCipheriv(ENCRYPTION_ALGORITHM, key, IV);
+  const iv = crypto.randomBytes(16);
+  const stream = crypto.createCipheriv(ENCRYPTION_ALGORITHM, key, iv);
+
+  return {
+    iv,
+    stream,
+  };
 }
 
 /**
- * @param {Buffer} key 
+ * @param {string | Buffer | NodeJS.TypedArray | DataView} key 
+ * @param {string | Buffer | NodeJS.TypedArray | DataView} iv 
  */
-const decryptStream = (key) => {
-  return crypto.createDecipheriv(ENCRYPTION_ALGORITHM, key, IV);
+const decryptStream = (key, iv) => {
+  return crypto.createDecipheriv(ENCRYPTION_ALGORITHM, key, iv);
 }
 
 /**
  * 
  * @param {string} original 
- * @param {Buffer} key 
+ * @param {string | Buffer | NodeJS.TypedArray | DataView} key 
  * @param {boolean} encrypt 
  */
-const cipherizeFile = (original, key, encrypt) => new Promise((resolve, reject) => {
+const cipherizeFile = (original, key, encrypt) => new Promise(async (resolve, reject) => {
   // Generate warshield file id
   const id = crypto.randomBytes(8).toString('hex');
   const targetname = `${original}.${id}.warshield`;
 
   // Error function
   const ERROR = err => {
+    console.log(err)
     fs.unlink(targetname, () => { });
     reject(err);
   }
 
-  const source = {
-    rs: fs.createReadStream(original).on('error', ERROR),
-    ws: null,
+  try {
+    if (encrypt) {
+      const rounds = Math.floor(Math.random() * (MAX_ROUNDS - MIN_ROUNDS) + MIN_ROUNDS);
+      const [derivedKey, salt] = await generateKey(key, rounds);
+
+      // Create cipher stream
+      const cipher = encryptStream(derivedKey);
+      cipher.stream.on('error', ERROR);
+
+      const source_rs = fs.createReadStream(original).on('error', ERROR);
+      const target_ws = fs.createWriteStream(targetname).on('error', ERROR);
+
+      if (!source_rs.readable || !target_ws.writable) {
+        return reject(false);
+      }
+
+      // Pipe original file into cipher and cipher into warshield file
+      const stream = source_rs.pipe(cipher.stream).pipe(target_ws);
+
+      stream.on('finish', () => {
+        const target_rs = fs.createReadStream(target_ws.path).on('error', ERROR);
+        const source_ws = fs.createWriteStream(source_rs.path).on('error', ERROR);
+
+        const tag = cipher.stream.getAuthTag();
+
+        source_ws.write(salt);
+        source_ws.write(cipher.iv);
+        source_ws.write(tag);
+        source_ws.write(Buffer.from(String(rounds)));
+
+        // Pipe warshield file into original file
+        target_rs.pipe(source_ws).on('finish', () => {
+          fs.unlink(target_ws.path, () => resolve(true));
+        });
+      });
+    } else {
+      const source_rs = fs.createReadStream(original, { end: 100 }).on('error', ERROR);
+      const target_ws = fs.createWriteStream(targetname).on('error', ERROR);
+
+      if (!source_rs.readable || !target_ws.writable) {
+        return reject(false);
+      }
+
+      source_rs.on('open', async fd => {
+        try {
+          const read = util.promisify(fs.read);
+          const { buffer: salt } = await read(fd, Buffer.alloc(64), 0, 64, 0);
+          const { buffer: iv } = await read(fd, Buffer.alloc(16), 0, 16, 64);
+          const { buffer: tag } = await read(fd, Buffer.alloc(16), 0, 16, 80);
+          const { buffer: rounds } = await read(fd, Buffer.alloc(4), 0, 4, 96);
+
+          const [derivedKey] = await generateKey(key, parseInt(rounds), salt);
+
+          // Create cipher stream
+          const decipher = decryptStream(derivedKey, iv);
+          decipher.on('error', ERROR);
+
+          decipher.setAuthTag(tag);
+
+          const data_rs = fs.createReadStream(original, { start: 100 }).on('error', ERROR);
+
+          // Pipe original file into cipher and cipher into warshield file
+          const stream = data_rs.pipe(decipher).pipe(target_ws);
+
+          stream.on('finish', () => {
+            const target_rs = fs.createReadStream(target_ws.path).on('error', ERROR);
+            const source_ws = fs.createWriteStream(source_rs.path).on('error', ERROR);
+
+            // Pipe warshield file into original file
+            target_rs.pipe(source_ws).on('finish', () => {
+              fs.unlink(target_ws.path, () => resolve(true));
+            });
+          });
+        } catch (e) {
+          ERROR(e);
+        }
+      });
+    }
+  } catch (e) {
+    ERROR(e);
   }
-
-  if (!source.rs.readable) {
-    return reject(false);
-  }
-
-  const target = {
-    rs: null,
-    ws: fs.createWriteStream(targetname).on('error', ERROR),
-  }
-
-  if (!target.ws.writable) {
-    return reject(false);
-  }
-
-  // Create cipher stream
-  const cipher = (encrypt ? encryptStream : decryptStream)(key);
-  cipher.on('error', ERROR);
-
-  // Pipe original file into cipher and cipher into warshield file
-  const stream = source.rs.pipe(cipher).pipe(target.ws);
-
-  stream.on('finish', () => {
-    target.rs = fs.createReadStream(target.ws.path).on('error', ERROR);
-    source.ws = fs.createWriteStream(source.rs.path).on('error', ERROR);
-
-    // Pipe warshield file into original file
-    target.rs.pipe(source.ws).on('finish', () => {
-      fs.unlink(target.ws.path, () => resolve(true));
-    });
-  });
 });
 
 module.exports = {
   generateKey,
-  encrypt,
-  decrypt,
   cipherizeFile,
 }
